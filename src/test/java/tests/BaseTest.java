a105edb0bbdc547fe4c9ce418e898217a29f24f7
Titus Fortner <titusfortner@gmail.com>
2020-03-18 15:56:32 -0500

    example test suite

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,39 @@
+package test.java.tests;
+
+import com.saucelabs.framework.Browser;
+import com.saucelabs.framework.pages.PageObject;
+import com.saucelabs.saucebindings.SauceOptions;
+import com.saucelabs.saucebindings.SauceSession;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BaseTest implements SessionManager {
+    public SauceSession session;
+
+    @Rule
+    public SauceTestWatcher testWatcher = new SauceTestWatcher(this);
+
+    @Rule
+    public TestName name = new TestName() {
+        public String getMethodName() {
+            return String.format("%s", super.getMethodName());
+        }
+    };
+
+    @Before
+    public void setup() {
+        SauceOptions options = new SauceOptions();
+        options.setName(name.getMethodName());
+        session = new SauceSession(options);
+        RemoteWebDriver driver = session.start();
+        Browser browser = new Browser(driver);
+        PageObject.setBrowser(browser);
+    }
+
+    @Override
+    public SauceSession getSession() {
+        return session;
+    }
+}
---
+++ src/test/java/tests/SessionManager.java
@@ -0,0 +1,8 @@
+package test.java.tests;
+
+
+import com.saucelabs.saucebindings.SauceSession;
+
+public interface SessionManager {
+    SauceSession getSession();
+}
